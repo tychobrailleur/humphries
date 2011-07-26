8bd58e52ab7e3fcecb94f16c61af2898351dd9b1
pierre <pierre@pierre-ThinkPad-T410.(none)>
2011-07-26 14:42:12 +0200

    Premiers tests et création du model Ticket

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -4,13 +4,17 @@ import play.*;
 import play.mvc.*;
 
 import java.util.*;
+import models.Ticket;
+import play.db.jpa.GenericModel;
 
 //import models.*;
 
 public class Application extends Controller {
 
     public static void index() {
-        render();
+        List<Ticket> tickets = Ticket.all().fetch();
+        render(tickets);
     }
 
+    
 }
