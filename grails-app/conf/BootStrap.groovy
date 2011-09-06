67908adb0949d66374c7c1ab2e3c47b2e8682dab
Sébastien Le Callonnec <sebastien@weblogism.com>
2011-09-06 23:08:33 +0100

    Format and added a comment

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,16 +1,22 @@
-import org.humphries.Ticket
-import org.humphries.Tag
-import org.humphries.Project
+import org.humphries.*
+
+/*
+
+  BootStrap initialises the test data by inserting pre-defined records in the 
+  database.
+
+*/
 
 class BootStrap {
 
     def init = { servletContext ->
-		def humphriesProject = new Project(name: "Humphries", code: "HUMP")
+        def humphriesProject = new Project(name: "Humphries", code: "HUMP")
         def introTag = new Tag(name: "Intro")
-		if (!Project.count()) {
-			humphriesProject.save(failOnError: true)
-		}
-		
+ 
+        if (!Project.count()) {
+            humphriesProject.save(failOnError: true)
+        }
+
         if (!Tag.count()) {
             new Tag(name: "Help wanted").save(failOnError: true)
             introTag.save(failOnError: true)
@@ -18,10 +24,10 @@ class BootStrap {
         
         if (!Ticket.count()) {
             new Ticket(name: "Grails spike",
-				reference: "HUMP-1",
-				description: "Exploratory spike on grails",
-				project: humphriesProject,
-				tag: introTag).save(failOnError: true)
+                reference: "HUMP-1",
+                description: "Exploratory spike on grails",
+                project: humphriesProject,
+                tag: introTag).save(failOnError: true)
         }
     }
     def destroy = {
