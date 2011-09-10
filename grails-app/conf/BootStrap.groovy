4d5259638d2d3f63f42968f553f114b95e2fa511
Sébastien Le Callonnec <sebastien@weblogism.com>
2011-09-10 10:51:05 +0100

    Revert "Revert "Merge branch 'grails'""
    This reverts commit 8055f5081a3624f0731992a77d8f7096a590c77b.

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,16 +1,34 @@
-import org.humphries.Ticket
-import org.humphries.Tag
-import org.humphries.Project
+import org.humphries.*
+import org.humphries.auth.*
+
+/*
+
+  BootStrap initialises the test data by inserting pre-defined records in the 
+  database.
+
+*/
 
 class BootStrap {
 
     def init = { servletContext ->
-		def humphriesProject = new Project(name: "Humphries", code: "HUMP")
+        def humphriesProject = new Project(name: "Humphries", code: "HUMP")
         def introTag = new Tag(name: "Intro")
-		if (!Project.count()) {
-			humphriesProject.save(failOnError: true)
-		}
-		
+        
+        if (!User.count()) {
+            def userPermission = new Permission(authority: 'USER').save()
+            
+            def password = 'password'
+            
+            [pierre: 'Pierre Rust', seb: 'Sébastien Le Callonnec'].each { username, displayName ->
+                def user = new User(username: username, displayName: displayName, password: password, enabled: true).save()
+                UserPermission.create(user, userPermission, true)
+            }
+        }
+ 
+        if (!Project.count()) {
+            humphriesProject.save(failOnError: true)
+        }
+
         if (!Tag.count()) {
             new Tag(name: "Help wanted").save(failOnError: true)
             introTag.save(failOnError: true)
@@ -18,12 +36,15 @@ class BootStrap {
         
         if (!Ticket.count()) {
             new Ticket(name: "Grails spike",
-				reference: "HUMP-1",
-				description: "Exploratory spike on grails",
-				project: humphriesProject,
-				tag: introTag).save(failOnError: true)
+                reference: "HUMP-1",
+                description: "Exploratory spike on grails",
+                project: humphriesProject,
+                creator: User.get(1),
+                tag: introTag).save(failOnError: true)
         }
     }
+
     def destroy = {
     }
 }
+
---
+++ grails-app/domain/org/humphries/auth/Permission.groovy
@@ -0,0 +1,14 @@
+package org.humphries.auth
+
+class Permission {
+
+    String authority
+
+    static mapping = {
+        cache true
+    }
+
+    static constraints = {
+        authority blank: false, unique: true
+    }
+}
