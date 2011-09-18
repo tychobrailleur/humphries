c44f1adb090cf37ebff4fa4e60b24b0c416586a7
Sébastien Le Callonnec <sebastien@weblogism.com>
2011-09-18 18:44:54 +0100

    Added cucumber tests for authentication

--- features/step_definitions/user_steps.rb
+++ features/step_definitions/user_steps.rb
@@ -5,3 +5,33 @@ Given /^I am logged in as (.*)$/ do |role|
   click_button 'Login'
 end
 
+Given /^I am on the home page$/ do 
+  visit 'http://localhost:8080/humphries'
+end
+
+When /^I authenticate as "([^"]+)"$/ do |username|
+  click_link 'Login'
+  fill_in 'username', :with => username
+  fill_in 'password', :with => 'password'
+  click_button 'Login'
+end
+
+When /^I sign out$/ do
+  visit 'http://localhost:8080/humphries/logout'
+end
+
+Then /^I should see I am logged in as "([^"]+)"$/ do |username|
+  visit 'http://localhost:8080/humphries'
+  response.should contain("Hiya, #{username}")
+  response.should contain("Sign out")
+  response.should_not contain("Login")
+end
+
+Then /^I should see I am not logged in as "([^"]+)"$/ do |username|
+  visit 'http://localhost:8080/humphries'
+  response.should_not contain("Hiya, #{username}")
+  response.should_not contain("Sign out")
+  response.should contain("Login")
+end
+
+
---
+++ features/user.feature
@@ -0,0 +1,15 @@
+@wip
+Feature: User Authentication
+  As a user
+  I want to be able to authenticate
+  So that I manage my tickets
+  
+  Scenario: Authenticate
+    Given I am on the home page
+    When I authenticate as "pierre"
+    Then I should see I am logged in as "pierre"
+    
+  Scenario: Sign out
+    Given I am logged in as "pierre"
+    When I sign out
+    Then I should see I am not logged in as "pierre"
