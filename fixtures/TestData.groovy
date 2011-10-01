8b268c58ed0048de884bc9d4985a72903b91f699
Sébastien Le Callonnec <sebastien@weblogism.com>
2011-10-01 10:04:26 +0100

    Using fixtures to define test data

---
+++ fixtures/TestData.groovy
@@ -0,0 +1,35 @@
+fixture {
+    humphries(org.humphries.Project, name: "Humphries", code: "HUMP")
+
+    // Tags
+    intro(org.humphries.Tag, name: 'Intro')
+    help(org.humphries.Tag, name: 'Help Wanted')
+
+
+    // Permissions
+    userPermission(org.humphries.auth.Permission, authority: 'USER_ROLE')
+    adminPermission(org.humphries.auth.Permission, authority: 'ADMIN_ROLE')
+
+    // User
+    pierre(org.humphries.auth.User, username: 'pierre', displayName: 'Pierre', password: 'password', enabled: true)
+    sebastien(org.humphries.auth.User, username: 'sebastien', displayName: 'Sébastien', password: 'password', enabled: true)
+
+    // Version
+    firstVersion(org.humphries.Version,  versionNum: '0.0.1', 
+                name: 'very first version',
+                creator: pierre, 
+                creationDate: new Date(),
+                project: humphries)
+
+    // Tickets
+    grailsSpike(org.humphries.Ticket, name: "Grails spike",
+                reference: "HUMP-1",
+                description: "Exploratory spike on grails",
+                project: humphries,
+                creator: pierre)
+}
+post {
+    org.humphries.auth.UserPermission.create(pierre, userPermission, true)
+    org.humphries.auth.UserPermission.create(pierre, adminPermission, true)
+    org.humphries.auth.UserPermission.create(sebastien, userPermission, true)
+}
--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,6 +1,8 @@
 import org.humphries.*
 import org.humphries.auth.*
 
+import grails.util.GrailsUtil
+import org.codehaus.groovy.grails.commons.GrailsApplication 
 /*
 
   BootStrap initialises the test data by inserting pre-defined records in the 
@@ -9,56 +11,14 @@ import org.humphries.auth.*
 */
 
 class BootStrap {
+    def fixtureLoader
 
     def init = { servletContext ->
-        def humphriesProject = new Project(name: "Humphries", code: "HUMP")
-        def introTag = new Tag(name: "Intro")
-        
-        if (!User.count()) {
-            def userPermission = new Permission(authority: 'USER_ROLE').save(flush:true)
-            def adminPermission = new Permission(authority: 'ADMIN_ROLE').save(flush: true)
-            
-            def password = 'password'
-            
-            [pierre: 'Pierre Rust', seb: 'Sébastien Le Callonnec'].each { username, displayName ->
-                def user = new User(username: username, displayName: displayName, password: password, enabled: true).save()
-                UserPermission.create(user, userPermission, true)
-                if (username == 'pierre') {
-                    UserPermission.create(user, adminPermission, true)
-                }
-            }
-        }
- 
-        if (!Project.count()) {
-//            humphriesProject.addToVersions(
-//                new Version( versionNum: '0.0.1', 
-//                    name: 'very first version',
-//                    creator: User.get(1), 
-//                    creationDate: new Date()))
-            humphriesProject.save(failOnError: true)
-        }
-        if (! Version.count()){
-            new Version( versionNum: '0.0.1', 
-                name: 'very first version',
-                creator: User.get(1), 
-                creationDate: new Date(),
-                project: humphriesProject).save(failOnError: true)
-        
-        }
+        boolean isProd = GrailsUtil.environment == GrailsApplication.ENV_PRODUCTION
 
-        if (!Tag.count()) {
-            new Tag(name: "Help wanted").save(failOnError: true)
-            introTag.save(failOnError: true)
-        }
-        
-        if (!Ticket.count()) {
-            new Ticket(name: "Grails spike",
-                reference: "HUMP-1",
-                description: "Exploratory spike on grails",
-                project: humphriesProject,
-                creator: User.get(1),
-                tag: introTag).save(failOnError: true)
-        }
+        if (!isProd) {
+	    fixtureLoader.load('TestData')
+	}
     }
 
     def destroy = {
